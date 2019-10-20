b2f62cc8fa6423de8e3ebb7819f17f5fee7c7648
Raul Ruiz <raul.ruiz@ebury.com>
2019-10-20 18:11:22 +0200

    Updated db password

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
         'USER': 'postgres',
-        'PASSWORD': 'postgres123',
+        'PASSWORD': 'postgres1234',
         'HOST': 'localhost',
         'PORT': '5432'
     }
