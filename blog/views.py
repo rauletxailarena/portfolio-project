c7764e2627136d39ff34c6b53612882de2c8ff54
Raul Ruiz <raul.ruiz@ebury.com>
2019-10-20 18:09:59 +0200

    Uploading portfolio

---
+++ blog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
+
+
+def allblogs(request):
+    blogs = Blog.objects
+    print("HEREEEEEEEEEEE" + str(blogs))
+    return render(request, 'blog/allblogs.html', {'blogs': blogs})
+
+
+def detail(request, blog_id):
+    detailBlog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/detail.html', {'blog': detailBlog})
